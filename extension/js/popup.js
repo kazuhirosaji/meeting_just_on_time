5755db780f33f7afeb76c7afe9abd764db5105db
Saita Tatsuya <saita.tatsuya@slogan.jp>
2016-01-09 05:18:30 +0900

    日時,

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -8,27 +8,43 @@ $(function() {
 
 		for(var i = 0; i < len; i++) {
 			if (data["result"][i].type == 1) {
-				title.append($("<li>").attr({"id":"date","class":data["result"][i].id}).text(data["result"][i].date));
+				title.append($("<li>").attr("class",data["result"][i].id).text("[" + data["result"][i].date +"]"+ data["result"][i].title));
+			}
+		}
+		setInterval( function(){
+			// 現在日時
+			var date = new Date();
+			// 現在時刻
+			//var time = new Date( getTime ).toLocaleTimeString();
+			//var hour = date.getHours();
+			//var min = date.getMinutes();
+
+			//parseInt( new Date() / 1000 )//
+
+			//var end_date = new Date()
+			$("#time").text(date);
+
+		}, 1000);
+
+
+		for(var i = 0; i < len; i++) {
+			if (data["result"][i].type == 1) {
+				var result_id = data["result"][i].id;
+			}
+			if (data["result"][i].type == 2 && data["result"][i].agenda != null) {
+				agenda.append($("<li>").attr({"class":result_id}).text(data["result"][i].agenda));
 			}
 		}
 
 		for(var i = 0; i < len; i++) {
 			if (data["result"][i].type == 1) {
-				$("#date").append($("#title").attr({"class":data["result"][i].id}).text(data["result"][i].title));
+				var result_id = data["result"][i].id;
 			}
-		}
-
-		for(var i = 0; i < len; i++) {
-			if (data["result"][i].type == 2 && data["result"][i].agenda != null) {
-				agenda.append($("<li>").attr({"class":data["result"][i].id}).text(data["result"][i].agenda));
-			}
-		}
-
-		for(var i = 0; i < len; i++) {
 			if (data["result"][i].type == 3) {
-				member.append($("<li>").attr({"class":data["result"][i].id}).text(data["result"][i].name));
+				member.append($("<li>").attr({"class":result_id}).text(data["result"][i].name));
 			}
 		}
 
 	});
+
 });
