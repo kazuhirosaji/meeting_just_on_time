3405b4a0f5efeb9315a470ff9aa0115b85852760
kazuhirosaji <kazuhirosaji@gmail.com>
2016-01-09 07:50:48 +0900

    Thank you KOMANBE

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -26,9 +26,44 @@ $(function() {
 
 		for(var i = 0; i < len; i++) {
 			if (data["result"][i].type == 3) {
-				member.append($("<li>").attr({"class":data["result"][i].id}).text(data["result"][i].name));
+				member.append($("<li>").attr({"id":"name" + i}).text(data["result"][i].name));
+				$("#name" + i).on("click", function(e){
+					sendToSpreadSheet($(this));
+				});;
+			}
+		}
+
+		for(var i = 0; i < len; i++) {
+			if (data["result"][i].type == 4) {
+				$("#" + data["result"][i].name_id).append('<span style="margin-left:20px;">参加</span>');
 			}
 		}
 
 	});
 });
+
+var sendToSpreadSheet = function(li) {
+	data = {type: "4", name: li.text(), attend:"10:11", name_id: li.attr('id')};
+	console.log(data);
+
+    $.ajax({
+        url: 'https://sheetsu.com/apis/04fbd25d',
+        data: data,
+        dataType: 'json',
+        type: 'POST',
+        
+        // place for handling successful response
+        // showing (redirecting to) something like /thanks.html
+        //page could be a good idea
+        success: function(data) {
+            console.log(data);
+        },
+        
+        // handling error response
+        error: function(data) {
+            console.log(data);
+        }
+    });
+
+    return false;
+};
---
+++ extension/js/post.js
@@ -0,0 +1,60 @@
+// jQuery snippet for changing HTML from into JSON
+(function ($) {
+    $.fn.serializeFormJSON = function () {
+
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
+$('.names').click(function(e) {
+    // prevent default submiting form
+    e.preventDefault();
+    sendToSpreadSheet();
+});
+
+var sendToSpreadSheet = function() {    
+    var data = $('#row' + loop).serializeFormJSON();
+    if (data.title == "" || data.agenda == "" || data.name == "") {
+        console.log(data);
+        loop += 1;
+        sendToSpreadSheet(); 
+        return false;
+    }
+    console.log(data);
+    console.log(loop);
+
+    $.ajax({
+        url: 'https://sheetsu.com/apis/04fbd25d',
+        data: data,
+        dataType: 'json',
+        type: 'POST',
+        
+        // place for handling successful response
+        // showing (redirecting to) something like /thanks.html
+        //page could be a good idea
+        success: function(data) {
+            console.log(data);
+            loop += 1;
+            sendToSpreadSheet();
+        },
+        
+        // handling error response
+        error: function(data) {
+            console.log(data);
+        }
+    });
+
+    return false;
+};
